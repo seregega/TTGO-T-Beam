9b121716546ba1a18794448532eaefebea904c02
Sergei <sergega@me.com>
2023-01-04 17:23:26 +0300

    add lora

--- plat_test/project/tbeam_blink/src/main.cpp
+++ plat_test/project/tbeam_blink/src/main.cpp
@@ -14,8 +14,21 @@ Also get TinyGPS++ library from:
 https://github.com/mikalhart/TinyGPSPlus
 ******************************************/
 
+#include <LoRa.h>
+#include <SPI.h> // include libraries
 #include <TinyGPS++.h>
 #include <axp20x.h>
+
+// lora section
+const int SCK_pin = 5;   // GPIO5  -- SX1278's SCK
+const int MISO_pin = 19; // GPIO19 -- SX1278's MISnO
+const int MOSI_pin = 27; // GPIO27 -- SX1278's MOSI
+const int SS_pin = 18;   // GPIO18 -- SX1278's CS
+const int RST_pin = 14;  // GPIO14 -- SX1278's RESET
+const int DI0_pin = 26;  // GPIO26 -- SX1278's IRQ(Interrupt Request)
+// #define BAND 868E6
+
+const long lora_frequency_BAND = 850125000; // LoRa Frequency
 
 TinyGPSPlus gps;
 HardwareSerial GPS(1);
@@ -34,12 +47,36 @@ void setup()
     Serial.println("AXP192 Begin FAIL");
   }
   axp.EnableCoulombcounter();
+  axp.enableChargeing(1);
+  axp.adc1Enable(AXP202_VBUS_VOL_ADC1 |
+                     AXP202_VBUS_CUR_ADC1 |
+                     AXP202_BATT_CUR_ADC1 |
+                     AXP202_BATT_VOL_ADC1 |
+                     AXP202_ACIN_VOL_ADC1 |
+                     AXP202_ACIN_CUR_ADC1 |
+                     AXP202_APS_VOL_ADC1 |
+                     AXP202_TS_PIN_ADC1,
+                 true);
+  // axp.adc2Enable(AXP202_);
+  // axp.enableDC2VRC()
   axp.setPowerOutPut(AXP192_LDO2, AXP202_ON);
   axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);
   axp.setPowerOutPut(AXP192_DCDC2, AXP202_ON);
   axp.setPowerOutPut(AXP192_EXTEN, AXP202_ON);
   axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON);
+  axp.setDCDC1Voltage(3300);
   GPS.begin(9600, SERIAL_8N1, 34, 12); // 17-TX 18-RX
+
+  SPI.begin(SCK_pin, MISO_pin, MOSI_pin, SS_pin);
+  LoRa.setPins(SS_pin, RST_pin, DI0_pin);
+  if (!LoRa.begin(lora_frequency_BAND))
+  {
+    Serial.println("Starting LoRa failed!");
+    while (1)
+      ;
+  }
+  LoRa.setFrequency(850125000);
+  //LoRa.setSignalBandwidth()
 }
 
 static void smartDelay(unsigned long ms)
@@ -54,6 +91,7 @@ static void smartDelay(unsigned long ms)
 
 void loop()
 {
+  static uint8_t counter = 0;
   Serial.print("getBattVoltage  : ");
   Serial.println(axp.getBattVoltage(), 5);
 
@@ -68,10 +106,18 @@ void loop()
   Serial.println(axp.getBattDischargeCoulomb(), 5);
   Serial.print("getCoulombData  : ");
   Serial.println(axp.getCoulombData(), 5);
-Serial.print("getSysIPSOUTVoltage  : ");
+  Serial.print("getSysIPSOUTVoltage  : ");
   Serial.println(axp.getSysIPSOUTVoltage(), 5);
-  
-  
+
+  Serial.print("getAcinVoltage  : ");
+  Serial.println(axp.getAcinVoltage(), 5);
+  Serial.print("getAcinCurrent  : ");
+  Serial.println(axp.getAcinCurrent(), 5);
+
+  Serial.print("getVbusVoltage  : ");
+  Serial.println(axp.getVbusVoltage(), 5);
+  Serial.print("getVbusCurrent  : ");
+  Serial.println(axp.getVbusCurrent(), 5);
 
   Serial.print("getTemp  : ");
   Serial.println(axp.getTemp(), 5);
@@ -94,6 +140,15 @@ Serial.print("getSysIPSOUTVoltage  : ");
   Serial.print("Speed     : ");
   Serial.println(gps.speed.kmph());
   Serial.println("**********************");
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("\0\0\0");
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  ++counter;
 
   smartDelay(1000);
 
